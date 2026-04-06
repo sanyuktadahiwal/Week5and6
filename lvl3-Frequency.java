86a81687e6c40f3be7c8363dd58d95357dee248f
sanyuktadahiwal <sanyuktadahiwal7@gmail.com>
2026-04-06 12:11:50 +0530

    Level 3: Problem 1 - Digit Frequency

---
+++ lvl3-Frequency.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class lvl3-Frequency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number: ");
+        String numberStr= sc.nextLine();
+        int[] frequency = new int[10];
+        for (int i=0; i<numberStr.lenght(); i++){
+            char ch = numberStr.charAr(i);
+            if(Character.idsDigit(ch)){
+                int digit = ch - '0';
+                frequency[digit]++;
+            }
+        }
+        System.out.println("Digit Frequency Table: ");
+        for (int i=0; i<10; i++){
+            if(frequency[i]>0){                
+                System.out.println("Digist " + i + " Occurs " + frequency[i] + " times.");
+            }
+        }
+        sc.close();
+    }
+}
